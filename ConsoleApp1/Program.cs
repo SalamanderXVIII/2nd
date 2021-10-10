3f45fa39eb2a2a94d653ac85db38fa6087097dbc
Андрей Гребенюк <andreygrebenyuk04@gmail.com>
2021-10-10 16:33:41 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Высокий уровень 2-й вариант.
+
+            Console.WriteLine("Введите значения x, c, t: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+            double t = Convert.ToDouble(Console.ReadLine());
+            double L = Math.Pow((1 / Math.Tan(c)), 2) + (2*x*x + 5) / Math.Sqrt(c + t);
+            Console.WriteLine("L={0:F2}", L);
+        }
+    }
+}
